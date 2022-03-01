7aa4e4defb4cb21fdec6e71608ff574c64105619
Damian Tarnawsky <damian@ionic.io>
2022-03-01 07:33:39 -0800

    (fix) Replace shorturl.at links with full urls

--- src/recommendations.ts
+++ src/recommendations.ts
@@ -990,12 +990,12 @@ export async function reviewProject(folder: string, context: vscode.ExtensionCon
 	);
 
 	// Adobe Mobiles services deprecation
-	project.deprecatedPlugin('adobe-mobile-services', 'Mobile Services reaches end-of-life on December 31, 2022', 'https://shorturl.at/lnpDT');
+	project.deprecatedPlugin('adobe-mobile-services', 'Mobile Services reaches end-of-life on December 31, 2022', 'https://experienceleague.adobe.com/docs/mobile-services/using/eol.html?lang=en');
 
 	// App Center deprecated Cordova SDK
-	project.deprecatedPlugin('cordova-plugin-appcenter-analytics', 'App Center is deprecating support for Cordova SDK in April 2022', 'https://shorturl.at/irzIL');
-	project.deprecatedPlugin('cordova-plugin-appcenter-crashes', 'App Center is deprecating support for Cordova SDK in April 2022', 'https://shorturl.at/irzIL');
-	project.deprecatedPlugin('cordova-plugin-appcenter-shared', 'App Center is deprecating support for Cordova SDK in April 2022', 'https://shorturl.at/irzIL');
+	project.deprecatedPlugin('cordova-plugin-appcenter-analytics', 'App Center is deprecating support for Cordova SDK in April 2022', 'https://devblogs.microsoft.com/appcenter/announcing-apache-cordova-retirement');
+	project.deprecatedPlugin('cordova-plugin-appcenter-crashes', 'App Center is deprecating support for Cordova SDK in April 2022', 'https://devblogs.microsoft.com/appcenter/announcing-apache-cordova-retirement');
+	project.deprecatedPlugin('cordova-plugin-appcenter-shared', 'App Center is deprecating support for Cordova SDK in April 2022', 'https://devblogs.microsoft.com/appcenter/announcing-apache-cordova-retirement');
 
 	project.deprecatedPlugin('@ionic-enterprise/offline-storage', 'Replace this plugin with @ionic-enterprise/secure-storage');
 
