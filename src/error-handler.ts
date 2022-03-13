7b5274d47d5d324728a23d04d9ffb2bf169ac602
Damian Tarnawsky <damian@ionic.io>
2022-03-13 11:24:23 -0700

    (feat) Assistance for react and typescript build errors

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -10,8 +10,6 @@ interface ErrorLine {
 }
 
 export async function handleError(error: string, logs: Array<string>, folder: string): Promise<string> {
-
-
 	if (error.includes('ionic: command not found')) {
 		const selection = await vscode.window.showErrorMessage('The Ionic CLI is not installed. Get started by running npm install -g @ionic/cli at the terminal.', 'More Information');
 		vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('https://ionicframework.com/docs/intro/cli#install-the-ionic-cli'));
@@ -40,8 +38,10 @@ function extractErrors(errorText: string, logs: Array<string>): Array<ErrorLine>
 
 	if (logs.length > 0) {
 		// Look for code lines
-		let line = undefined;
+		let line = undefined; // Lint style errors
+		let rcline = undefined; // React style Typescript errors
 		for (const log of logs) {
+			// Lint style errors
 			if (log.endsWith('.ts')) {
 				line = log;
 			} else {
@@ -54,6 +54,23 @@ function extractErrors(errorText: string, logs: Array<string>): Array<ErrorLine>
 
 				}
 			}
+
+			// React style errors
+			if (log.startsWith('TypeScript error in ')) {
+				rcline = log;
+			} else {
+				if (rcline) {
+					const errormsg = log.trim();
+					errors.push(extractTypescriptErrorFrom(rcline, errormsg));
+					rcline = undefined;
+				}
+			}
+
+			// React synax error
+			// SyntaxError: /Users/damian/Code/demo-intune-react/src/pages/Login.tsx: 'await' is only allowed within async functions and at the top levels of modules. (29:19)
+			if (log.startsWith('SyntaxError:')) {
+				errors.push(extractSyntaxError(log));
+			}
 		}
 	}
 
@@ -61,30 +78,35 @@ function extractErrors(errorText: string, logs: Array<string>): Array<ErrorLine>
 		const lines = errorText.split('\n');
 		for (const line of lines) {
 			if (line.startsWith('Error: ')) {
-				try {
-					// Parse an error like this one for the line, position and error message
-					// Error: src/app/app.module.ts:18:3 - error TS2391: Function implementation is missing or not immediately following the declaration.
-					const codeline = line.replace('Error: ', '').split(':')[0];
-					const args = line.split(':');
-					const linenumber = parseInt(args[2]) - 1;
-					const position = parseInt(args[3].substring(0, args[3].indexOf(' ')) + 2) - 1;
-					const errormsg = line.substring(line.indexOf('- ', codeline.length + 7));
-					errors.push({ line: linenumber, position: position, uri: codeline, error: errormsg });
-				} catch {
-					// Couldnt parse the line. Continue
-				}
+				errors.push(extractErrorFrom(line));
 			}
 		}
 	}
 	return errors;
 }
 
+// Parse an error like this one for the line, position and error message
+// Error: src/app/app.module.ts:18:3 - error TS2391: Function implementation is missing or not immediately following the declaration.
+function extractErrorFrom(line: string): ErrorLine {
+	try {
+		const codeline = line.replace('Error: ', '').split(':')[0];
+		const args = line.split(':');
+		const linenumber = parseInt(args[2]) - 1;
+		const position = parseInt(args[3].substring(0, args[3].indexOf(' ')) + 2) - 1;
+		const errormsg = line.substring(line.indexOf('- ', codeline.length + 7));
+		return { line: linenumber, position: position, uri: codeline, error: errormsg };
+	} catch {
+		// Couldnt parse the line. Continue
+	}
+}
+
+
 async function handleErrorLine(number: number, errors: Array<ErrorLine>, folder: string) {
-	const msg = extractErrorMessage(errors[number].error);
+	if (!errors[number]) return;
 	const nextButton = (number + 1 == errors.length) ? undefined : 'Next';
 	const prevButton = (number == 0) ? undefined : 'Previous';
 	const title = (errors.length > 1) ? `Error ${number + 1} of ${errors.length}: ` : '';
-	vscode.window.showErrorMessage(`${title}${msg}`, prevButton, nextButton, 'Ok').then((result) => {
+	vscode.window.showErrorMessage(`${title}${errors[number].error}`, prevButton, nextButton, 'Ok').then((result) => {
 		if (result == 'Next') {
 			handleErrorLine(number + 1, errors, folder);
 			return;
@@ -107,7 +129,9 @@ async function handleErrorLine(number: number, errors: Array<ErrorLine>, folder:
 	vscode.commands.executeCommand('revealLine', { lineNumber: myPos.line, at: 'bottom' });
 }
 
-// Given "  13:1  error blar" return "blar"
+// Extract error message from a line error line:
+// eg "  13:1  error blar"
+// return "blar"
 function extractErrorMessage(msg: string): string {
 	try {
 		const pos = parsePosition(msg);
@@ -141,4 +165,39 @@ function parsePosition(msg: string): vscode.Position {
 		}
 	}
 	return new vscode.Position(0, 0);
+}
+
+
+// Extract code filename, line number, position
+// TypeScript error in /Users/damian/Code/demo-intune-react/src/components/ExploreContainer.tsx(5,7):
+function extractTypescriptErrorFrom(msg: string, errorText: string): ErrorLine {
+	try {
+		msg = msg.replace('TypeScript error in ', '');
+		const filename = msg.substring(0, msg.lastIndexOf('('));
+		const args = msg.substring(msg.lastIndexOf('(') + 1).split(',');
+		const linenumber = parseInt(args[0]);
+		const position = parseInt(args[1].replace('):', ''));
+		return { line: linenumber, position: position, error: errorText, uri: filename };
+	} catch
+	{
+		return;
+	}
+}
+
+// Extract code filename, line number, position
+// SyntaxError: /Users/damian/Code/demo-intune-react/src/pages/Login.tsx: 'await' is only allowed within async functions and at the top levels of modules. (29:19)
+function extractSyntaxError(msg: string): ErrorLine {
+	try {
+		msg = msg.replace('SyntaxError: ', '');
+		const filename = msg.substring(0, msg.indexOf(':'));
+		const args = msg.substring(msg.lastIndexOf('(') + 1).split(':');
+		const linenumber = parseInt(args[0]);
+		const position = parseInt(args[1].replace(')', ''));
+		let errorText = msg.substring(msg.indexOf(':') + 1);
+		errorText = errorText.substring(0, errorText.lastIndexOf('(')).trim();
+		return { line: linenumber, position: position, error: errorText, uri: filename };
+	} catch
+	{
+		return;
+	}
 }
