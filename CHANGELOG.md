bb212e4e8225d3b6f18de83904f9c5532a37eeea
Damian Tarnawsky <damian@ionic.io>
2022-03-24 17:34:32 -0700

    (fix) Fix for Angular migrations when angular/cli is ony installed locally

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@
 - Fix for "upgrade all packages"
 - Fix upgrade to version of package if package is on latest
 - Fix when esc is pressed when selecting device
+- Fix for Angular migrations when angular/cli is ony installed locally
 - Better error message when shell is not set to /bin/zsh
 
 ### Version 1.3.1
--- src/rules-angular-migrate.ts
+++ src/rules-angular-migrate.ts
@@ -14,20 +14,20 @@ export function angularMigrate(latestVersion: string): Tip {
 		'Upgrade Angular',
 		'Updates your application and its dependencies to the latest version using "ng update". Make sure you have committed your code before trying an upgrade.',
 		TipType.Run, undefined,
-		'ng update @angular/cli @angular/core --allow-dirty --force', `Upgrade to v${latest.major}`, undefined, 'https://angular.io/cli/update').showProgressDialog();		
+		'npx ng update @angular/cli @angular/core --allow-dirty --force', `Upgrade to v${latest.major}`, undefined, 'https://angular.io/cli/update').showProgressDialog();		
 
 	// Upgrade option to next major
 	if (next == latest.major) {
 		// If we are 1 major version behind then update to latest same major (eg 12.0.1 -> 12.2.6)
 		tip.setSecondCommand(
 			`Upgrade to latest v${current.major}`,
-			`ng update @angular/cli@${current.major} @angular/core@${current.major} --allow-dirty --force`
+			`npx ng update @angular/cli@${current.major} @angular/core@${current.major} --allow-dirty --force`
 		);		
 	} else if (next <= latest.major) {
 		// Upgrade to next major
 		tip.setSecondCommand(
 			`Upgrade to v${next}`,
-			`ng update @angular/cli@${next} @angular/core@${next} --allow-dirty --force`
+			`npx ng update @angular/cli@${next} @angular/core@${next} --allow-dirty --force`
 		);
 	}
 	return tip;
