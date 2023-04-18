3aa28c5a968c79615bf98c85e6d9d625b8c8bb27
dtarnawsky <damian@ionic.io>
2023-04-18 10:26:15 -0700

    chore: prep for release

--- src/features.ts
+++ src/features.ts
@@ -1,5 +1,5 @@
 // Feature Flags for experimental options
 export const features = {
   debugAndroid: true, // Whether debugging for Android is turned on
-  pluginExplorer: true, // Whether the plugin explorer is shown
+  pluginExplorer: false, // Whether the plugin explorer is shown
 };
