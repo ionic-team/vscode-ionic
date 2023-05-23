c00638cdf60a664a70f3c00ce77076f62734aeb4
dtarnawsky <damian@ionic.io>
2023-05-23 14:28:58 -0700

    feat: tips only shown every 30 days

--- src/features.ts
+++ src/features.ts
@@ -1,18 +1,19 @@
 import { window } from 'vscode';
-import { WorkspaceSetting, getSetting, setSetting } from './workspace-state';
+import { GlobalSetting, getGlobalSetting, getSetting, setGlobalSetting, setSetting } from './workspace-state';
 import { alt } from './utilities';
 
 // Feature Flags for experimental options
 export const features = {
   debugAndroid: true, // Whether debugging for Android is turned on
-  pluginExplorer: false, // Whether the plugin explorer is shown
+  pluginExplorer: true, // Whether the plugin explorer is shown
 };
 
 export function showTips() {
-  const shortcuts = '[shortcuts]';
-  const tips = getSetting(WorkspaceSetting.tips);
-  if (!tips?.includes(shortcuts)) {
+  const tips = getGlobalSetting(GlobalSetting.lastTipsShown);
+  const shownAt = tips ? Date.parse(tips) : 0;
+  const days = (new Date().getTime() - shownAt) / (1000 * 3600 * 24);
+  if (days > 30) {
     window.showInformationMessage(`Ionic Tip: Press ${alt('D')} to debug your app and ${alt('R')} to run it!`, 'OK');
-    setSetting(WorkspaceSetting.tips, tips + shortcuts);
+    setGlobalSetting(GlobalSetting.lastTipsShown, new Date().toISOString());
   }
 }
