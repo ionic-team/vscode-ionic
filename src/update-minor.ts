3290c3d7a8a86c430f923f0b0f5e377e7a6ea48e
dtarnawsky <damian@ionic.io>
2023-07-20 08:02:42 -0700

    fix: yarn 1.x fixes

--- src/update-minor.ts
+++ src/update-minor.ts
@@ -1,36 +1,49 @@
-import { mkdtempSync, rmSync, writeFileSync } from 'fs';
+import { copyFileSync, existsSync, mkdtempSync, rmSync, writeFileSync } from 'fs';
 import { tmpdir } from 'os';
 import { join } from 'path';
 import { clearOutput, writeError, writeIonic } from './logging';
-import { npmInstall, outdatedCommand } from './node-commands';
+import { PackageManager, npmInstall, npmInstallAll, outdatedCommand } from './node-commands';
 import { NpmOutdatedDependency } from './npm-model';
 import { Project } from './project';
 import { getRunOutput, run, RunResults, showProgress } from './utilities';
 import * as vscode from 'vscode';
+import { fixYarnGarbage } from './monorepo';
 
 export async function updateMinorDependencies(project: Project, packages: object): Promise<void> {
   const channel = clearOutput();
   try {
     writeIonic(`Checking for minor updates for ${Object.keys(packages).length} dependencies`);
-    const pkg = { dependencies: {}, name: 'tmp' };
+    const pkg = { dependencies: {}, name: 'tmp', license: 'MIT' };
     for (const library of Object.keys(packages).sort()) {
       pkg.dependencies[library] = `^${packages[library].version}`;
     }
     const tmpDir = mkdtempSync(join(tmpdir(), 'vscode.ionic.ext'));
     const tmpFile = join(tmpDir, 'package.json');
     writeFileSync(tmpFile, JSON.stringify(pkg, undefined, 2));
-    const data = await getRunOutput(outdatedCommand(project.packageManager), tmpDir);
-    const out = JSON.parse(data);
+
+    if (project.packageManager == PackageManager.yarn) {
+      writeIonic(`This may take a moment (as you are using yarn)`);
+      await getRunOutput(npmInstallAll(), tmpDir);
+    }
+    let data = await getRunOutput(outdatedCommand(project.packageManager), tmpDir, undefined, true);
+    data = fixYarnGarbage(data, project.packageManager);
     let count = 0;
     const updates = [];
-    for (const library of Object.keys(packages).sort()) {
-      const dep: NpmOutdatedDependency = out[library];
-      if (dep && packages[library].version !== dep.wanted) {
-        channel.appendLine(`${library} ${packages[library].version} → ${dep.wanted}`);
-        updates.push(`${library}@${dep.wanted}`);
-        count++;
+    try {
+      const out = JSON.parse(data);
+      for (const library of Object.keys(packages).sort()) {
+        const dep: NpmOutdatedDependency = out[library];
+        if (dep && packages[library].version !== dep.wanted) {
+          channel.appendLine(`${library} ${packages[library].version} → ${dep.wanted}`);
+          updates.push(`${library}@${dep.wanted}`);
+          count++;
+        }
       }
+    } catch {
+      writeError(`${outdatedCommand(project.packageManager)} returned invalid json.`);
+      writeError(data);
     }
+
     rmSync(tmpFile);
 
     if (count == 0) {
