1be290000888c2f2f49c5a71e88bee2cc091170c
damian.ionic <damian@ionic.io>
2024-01-25 16:11:05 -0800

    fix: cleanup auto imports

--- src/imports-angular.ts
+++ src/imports-angular.ts
@@ -1,6 +1,7 @@
 import { existsSync } from 'fs';
-import { Range, TextDocument, Uri, WorkspaceEdit, commands, workspace } from 'vscode';
+import { Range, TextDocument, Uri, WorkspaceEdit, workspace } from 'vscode';
 import { toPascalCase } from './utilities';
+import { IonicComponents } from './imports-auto-fix';
 
 export async function autoFixAngularImports(document: TextDocument, component: string): Promise<boolean> {
   // Validate that the file changed was a .html file that also has a .ts file which uses @ionic standalone
@@ -8,8 +9,7 @@ export async function autoFixAngularImports(document: TextDocument, component: s
   const tsFile = document.fileName.replace(new RegExp('.html$'), '.ts');
   if (!existsSync(tsFile)) return false;
   const edit = new WorkspaceEdit();
-  const tsUri = Uri.file(tsFile);
-  const tsDoc = await workspace.openTextDocument(tsUri);
+  const tsDoc = await workspace.openTextDocument(Uri.file(tsFile));
 
   // const tsText = readFileSync(tsFile, 'utf-8');
   const tsText = tsDoc.getText();
@@ -18,91 +18,7 @@ export async function autoFixAngularImports(document: TextDocument, component: s
   if (!tsText.includes(`'@ionic/angular/standalone'`)) return false;
   if (!component.startsWith('ion-')) return false;
 
-  // Search for each Ionic Component in the template html and see if it is missing in the Typescript imports
-  const components = [
-    'ion-action-sheet',
-    'ion-accordion',
-    'ion-accordion-group',
-    'ion-alert',
-    'ion-badge',
-    'ion-breadcrumb',
-    'ion-button',
-    'ion-ripple-effect',
-    'ion-card',
-    'ion-card-content',
-    'ion-card-header',
-    'ion-card-subtitle',
-    'ion-card-title',
-    'ion-checkbox',
-    'ion-chip',
-    'ion-app',
-    'ion-content',
-    'ion-datetime',
-    'ion-datetime-button',
-    'ion-picker',
-    'ion-fab',
-    'ion-fab-button',
-    'ion-fab-list',
-    'ion-grid',
-    'ion-col',
-    'ion-row',
-    'ion-infinite-scroll',
-    'ion-infinite-scroll-content',
-    'ion-icon',
-    'ion-input',
-    'ion-textarea',
-    'ion-item',
-    'ion-item-divider',
-    'ion-item-group',
-    'ion-item-sliding',
-    'ion-item-options',
-    'ion-item-option',
-    'ion-label',
-    'ion-note',
-    'ion-list',
-    'ion-list-header',
-    'ion-avatar',
-    'ion-img',
-    'ion-split-pane',
-    'ion-modal',
-    'ion-backdrop',
-    'ion-nav',
-    'ion-nav-link',
-    'ion-popover',
-    'ion-loading',
-    'ion-progress-bar',
-    'ion-skeleton-text',
-    'ion-spinner',
-    'ion-radio',
-    'ion-radio-group',
-    'ion-range',
-    'ion-refresher',
-    'ion-refresher-content',
-    'ion-reorder',
-    'ion-reorder-group',
-    'ion-router',
-    'ion-router-link',
-    'ion-router-outlet',
-    'ion-route',
-    'ion-route-redirect',
-    'ion-searchbar',
-    'ion-segment',
-    'ion-segment-button',
-    'ion-tabs',
-    'ion-tab',
-    'ion-tab-bar',
-    'ion-tab-button',
-    'ion-toast',
-    'ion-toggle',
-    'ion-toolbar',
-    'ion-header',
-    'ion-footer',
-    'ion-title',
-    'ion-buttons',
-    'ion-back-button',
-    'ion-text',
-  ];
-  if (!components.includes(component)) {
+  if (!IonicComponents.includes(component)) {
     // Not a knowm Ionic Component
     return false;
   }
