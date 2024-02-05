d4d9df48a29518694fd40f0bbe5f986859b91443
damian.ionic <damian@ionic.io>
2024-02-05 08:29:13 -0800

    fix: auto imports

--- src/utilities.ts
+++ src/utilities.ts
@@ -194,6 +194,9 @@ export async function run(
         viewInEditor(localUrl);
         break;
       case WebConfigSetting.browser:
+        if (!externalUrl) {
+          openUri(localUrl);
+        }
         break;
       default: {
         //qrView(externalUrl);
