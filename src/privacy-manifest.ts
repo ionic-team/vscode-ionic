6b3948d9b32fd3c4445b7df8062191b79c29f371
damian.ionic <damian@ionic.io>
2024-03-21 06:38:09 -0700

    feat: add filesystem as an reason code

--- src/privacy-manifest.ts
+++ src/privacy-manifest.ts
@@ -14,6 +14,13 @@ export const privacyManifestRules = [
     reasons: ['CA92.1', '1C8F.1'],
   },
   {
+    plugin: '@capacitor/filesystem',
+    category: 'NSPrivacyAccessedAPICategoryFileTimestamp',
+    reasonUrl:
+      'https://developer.apple.com/documentation/bundleresources/privacy_manifest_files/describing_use_of_required_reason_api#4278393',
+    reasons: ['C617.1', 'DDA9.1', '3B52.1'], // FYI: 0A2A.1 is not applicable
+  },
+  {
     plugin: '@capacitor-community/mdm-appconfig',
     category: 'NSPrivacyAccessedAPICategoryUserDefaults',
     reasonUrl:
